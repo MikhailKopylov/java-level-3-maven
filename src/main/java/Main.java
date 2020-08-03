180b2a7444bd4e3e1870c416c8cf434cd65acd8f
Mikhail <MIKHAILVALKOPYLOV@YANDEX.Ru>
2020-08-03 18:53:29 +0300

    home work 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,9 @@
+import lesson6.Lesson6;
+
 public class Main {
 
+    public static void main(String[] args) {
+        Lesson6 lesson = new Lesson6();
+        lesson.getArrayAfterLastFour(new int[]{1, 2, 3, 4, 5, 6});
+    }
 }
